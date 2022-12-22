3c296c5678e83d0dde9c7424d9e37cbeafc13d65
unknown <danny@mewpot.com>
2022-12-22 21:55:35 +0900

    [Feat] Router.js 추가

--- planetarium_test/src/App.js
+++ planetarium_test/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import BasicExample from './components/nav'
-
+import Router from './router';
 function App() {
   return (
     <div className="App">
       <BasicExample/>
+      <Router/>
     </div>
   );
 }
---
+++ planetarium_test/src/router.js
@@ -0,0 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import EditIssue from './pages/editIssue';
+import Issues from './pages/Issues';
+import NewIssue from './pages/newIssue';
+
+export default function Router() {
+  return(
+    <Routes>
+      <Route path='/' element={<Link to='/issues'/>} />
+      <Route path='/issues/*' element={<Issues/>}>
+        {/* <Route path=':id' element={<Issue/>} /> */}
+        {/* <Route path='edit' element={<EditIssue/>}/> */}
+      </Route>
+      <Route path='/new' element={<NewIssue/>}/>
+    </Routes>
+  )
+}
