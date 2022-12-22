b30ca131db73de03271badf36a10d7e715f232a3
unknown <danny@mewpot.com>
2022-12-22 22:00:02 +0900

    [Feat] 이슈 컴포넌트 추가 - 이슈 개수 만큼 추가

--- planetarium_test/src/pages/Issue.js
+++ planetarium_test/src/pages/Issue.js
@@ -1,3 +1,28 @@
-export default function Issue() {
-  return (<></>)
+import { Badge, Card } from 'react-bootstrap';
+
+export default function Issue(props) {
+  const {title, body, labels} = props
+  const badgeBg = {
+    'documentation': 'primary',
+    'duplicate': 'secondary',
+    'good first issue': 'success',
+    'bug' : 'danger',
+    'invalid': 'warning',
+    'question': 'info',
+    'help wanted': 'light',
+    'enhancement': 'dark'
+  }
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          {body}
+        </Card.Text>
+        {labels.map((el, idx)=>
+          <Badge key={idx} bg={badgeBg[el.name]}>{el.name}</Badge>
+        )}
+      </Card.Body>
+    </Card>
+  )
 }
